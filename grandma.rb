506aa0403149260af1b30502f049c1beccb3d7c6
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 14:20:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,14 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(sentence)
+
+  if(sentence == sentence.upcase)
+    "NO, NOT SINCE 1938!"
+  elsif(sentence == "I LOVE YOU GRANDMA!")
+    "I LOVE YOU TOO PUMPKIN!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
