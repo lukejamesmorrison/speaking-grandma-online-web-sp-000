980c7a3422d328a47f99bd7cc2a0494c2ecc0dac
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 14:21:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(sentence)
-
-  if(sentence == sentence.upcase)
-    "NO, NOT SINCE 1938!"
-  elsif(sentence == "I LOVE YOU GRANDMA!")
+  
+  if(sentence == "I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
-  else
+  elsif(sentence == sentence.upcase)
+    "NO, NOT SINCE 1938!"
+  
     "HUH?! SPEAK UP, SONNY!"
   end
 end
